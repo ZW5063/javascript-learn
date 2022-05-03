e0203cb95e4a8532dd2744b2c192fc61e09ec95c
刘志玮 <383545510@qq.com>
2022-05-03 19:44:46 +0800

    day-8变量js

---
+++ js/变量.js
@@ -0,0 +1,10 @@
+// let num = value = 1;
+// let value;
+// num = 1;
+// value = 1;
+
+let num = 123, value = 321;
+let [num1, value1] = [123, '321'];
+
+// console.log(num, value);
+console.log(num1, value1);
