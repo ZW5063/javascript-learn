d934a41cd987e5afdd21a8cd513e72747eff8ff6
刘志玮 <383545510@qq.com>
2022-05-05 18:26:53 +0800

    day-8some

--- js/变量.js
+++ js/变量.js
@@ -3,8 +3,18 @@
 // num = 1;
 // value = 1;
 
-let num = 123, value = 321;
-let [num1, value1] = [123, '321'];
+// let num = 123, value = 321;
+// let [num1, value1] = [123, '321'];
 
 // console.log(num, value);
-console.log(num1, value1);
+// console.log(num1, value1);
+
+// let num = 88;
+// var num2 = Symbol('88');
+// var num2 = Symbol('87');
+
+// console.log(num, num2);
+// num = 99;
+// console.log(num);
+// var num;
+let num = 7;
