9d9ecf7fe2eb9416adef1b4d7eec6c84252c9d94
刘志玮 <383545510@qq.com>
2022-05-29 09:58:33 +0800

    123

---
+++ 43.export_import/export.js
@@ -0,0 +1,8 @@
+export default {
+    num: 2,
+    arr: [1, 2, 3],
+    obj: {
+        name: 'xm',
+        age: 18
+    }
+}
---
+++ 43.export_import/import.js
@@ -0,0 +1,3 @@
+import { default as unknow } from './export.js';
+console.log(unknow);
+
